b23546d7d77aba6c3aa164d8ec3263b728d4e7ee
usausa <machi.pon@gmail.com>
2017-04-11 21:03:35 +0900

    Update application name

--- Example.FormsApp/Example.FormsApp.Android/MainActivity.cs
+++ Example.FormsApp/Example.FormsApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Example.FormsApp.Droid
 {
-    [Activity(Label = "Example.FormsApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
