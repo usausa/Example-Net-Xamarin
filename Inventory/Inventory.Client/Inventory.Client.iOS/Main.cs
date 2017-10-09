fe04fcbc8b1449c07e2162e40b9a23c60420df91
usausa <machi.pon@gmail.com>
2017-10-09 19:54:43 +0900

    Add Analyzers

--- Inventory/Inventory.Client/Inventory.Client.iOS/Main.cs
+++ Inventory/Inventory.Client/Inventory.Client.iOS/Main.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
-using UIKit;
-
-namespace Inventory.Client.iOS
+﻿namespace Inventory.Client.iOS
 {
-	public class Application
-	{
-		// This is the main entry point of the application.
-		static void Main(string[] args)
-		{
-			// if you want to use a different Application Delegate class from "AppDelegate"
-			// you can specify it here.
-			UIApplication.Main(args, null, "AppDelegate");
-		}
-	}
+    using UIKit;
+
+    public static class Application
+    {
+        // This is the main entry point of the application.
+        public static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
 }
--- Inventory/Inventory.Client/Inventory.Client/MainPage.xaml.cs
+++ Inventory/Inventory.Client/Inventory.Client/MainPage.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-
-namespace Inventory.Client
+﻿namespace Inventory.Client
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
 }
