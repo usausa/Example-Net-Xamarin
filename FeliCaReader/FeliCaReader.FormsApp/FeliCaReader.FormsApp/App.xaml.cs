5a8c67c7c74885b2f26008326f5297468dc3d6a2
usausa <machi.pon@gmail.com>
2017-11-03 15:08:58 +0900

    Update project baseline

--- FeliCaReader/FeliCaReader.FormsApp/FeliCaReader.FormsApp/App.xaml.cs
+++ FeliCaReader/FeliCaReader.FormsApp/FeliCaReader.FormsApp/App.xaml.cs
@@ -1,34 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿[assembly: Xamarin.Forms.Xaml.XamlCompilation(Xamarin.Forms.Xaml.XamlCompilationOptions.Compile)]
 
 namespace FeliCaReader.FormsApp
 {
-	public partial class App : Application
-	{
-		public App ()
-		{
-			InitializeComponent();
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new FeliCaReader.FormsApp.MainPage();
-		}
+            MainPage = new MainPage();
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
--- FeliCaReader/FeliCaReader.FormsApp/FeliCaReader.FormsApp/MainPage.xaml.cs
+++ FeliCaReader/FeliCaReader.FormsApp/FeliCaReader.FormsApp/MainPage.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-
-namespace FeliCaReader.FormsApp
+﻿namespace FeliCaReader.FormsApp
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
 }
